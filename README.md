9390b31576eb44d35ad06fc50b6a5d90ebb88b15
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-07 16:13:53 -0400

    demo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # TodoMVC with XState
 
 Copied from [codesandbox](https://codesandbox.io/s/xstate-todomvc-33wr94qv1)
+
+```
+npm install
+npm run dev
+```
+
+Pick the Cypress spec file
+
+![Demo showing combination of model and DOM actions](images/demo.gif)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,28 +1,61 @@
 /// <reference types="cypress" />
-import { inspect } from "@xstate/inspect";
+import { inspect } from '@xstate/inspect'
 
 describe('TodoMVC', () => {
-  beforeEach(() => {
-    // let xstateFrame = Cypress.$('<iframe data-xstate></iframe>')
-    const thisFrame = window.parent.document.querySelector('.spec-iframe')
-    inspect({
-      // opens another Cypress browser window
-      // with state machine visualization
-      iframe: false
-      // iframe: thisFrame,
-      // url: ''
-      // iframe: xstateFrame[0]
-    });
-  })
+  // beforeEach(() => {
+  //   // let xstateFrame = Cypress.$('<iframe data-xstate></iframe>')
+  //   const thisFrame = window.parent.document.querySelector('.spec-iframe')
+  //   inspect({
+  //     // opens another Cypress browser window
+  //     // with state machine visualization
+  //     iframe: false
+  //     // iframe: thisFrame,
+  //     // url: ''
+  //     // iframe: xstateFrame[0]
+  //   });
+  // })
 
   it('works', () => {
+    const state = {}
     cy.visit('/', {
-      // onBeforeLoad(win) {
-      //   const iframe = Cypress.$('<iframe data-xstate></iframe>')
-      //   const inspectXState = inspect
-      //   debugger
-      //   win.eval(`inspectXState({iframe})`)
-      // }
+      onBeforeLoad(win) {
+        win.__xstate__ = {
+          register: (x) => {
+            state.xstate = x
+          },
+        }
+      },
+    })
+    // initially
+    cy.wrap(state)
+      .its('xstate.machine.context')
+      .should('deep.equal', {
+        todo: 'Learn state machines',
+        todos: [],
+      })
+      .wait(1000) // pause for demo
+
+    // set a different todo text
+    cy.wrap(state)
+      .its('xstate')
+      .invoke('send', { type: 'NEWTODO.CHANGE', value: 'Test using model' })
+      .wait(1000) // pause for demo
+
+    // and see it reflected in the DOM
+    cy.get('.new-todo')
+      .should('have.value', 'Test using model')
+      .then(() => {
+        cy.wrap(state)
+          .its('xstate')
+          .invoke('subscribe', (state, event) => cy.stub().as('events')(event))
+      })
+
+    // if we add the todo via DOM
+    cy.get('.new-todo').type('{enter}').wait(1000) // pause for demo
+    // then we will have the event in the state machine
+    cy.get('@events').should('have.been.calledWith', {
+      type: 'NEWTODO.COMMIT',
+      value: 'Test using model',
     })
   })
 })
